5490e49e85a60a597f8a6b0e581a78505d6bc08a
Miguel Gutierrez <migutie@gmail.com>
2020-11-19 12:08:00 -0500

    Formatación de archivos Node.js

--- luca_sky_precioenvivo.js
+++ luca_sky_precioenvivo.js
@@ -1,113 +1,115 @@
-/*main() will be run when you invoke this action
- *@param Cloud Functions actions accept a single parameter, which must be a JSON object.
- *@return The output of this action, which must be a JSON object.*/
+/*
+ * main() will be run when you invoke this action
+ * @param Cloud Functions actions accept a single parameter, which must be a JSON object.
+ * @return The output of this action, which must be a JSON object.
+ */
 
 function main(params) {
 
-    var request = require('request');
-    var ciudaddestino = "CTG";
-    var ciudadorigen = "BOG";
-    var fechaida = "2020-03-15";
-    var fechavuelta  = "";
-    var pasajeros = "1";
-    
+var request = require('request');
+var ciudaddestino = "CTG";
+var ciudadorigen = "BOG";
+var fechaida = "2020-03-15";
+var fechavuelta  = "";
+var pasajeros = "1";
 
-    var endPoints = {
-        'buildSearch': 'https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/pricing/v1.0/',
-        'getSearchData': 'https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/pricing/uk2/v1.0/'
-    };
-    
-    var headers = {
-        'X-RapidAPI-Host': "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
-        'X-RapidAPI-Key': "ccd0998f3amsh10e1fed69fc31afp14386ejsnb4fdf587810c"
-    };
-    
-    var formData = {
-        inboundDate: fechavuelta,
-        cabinClass: 'economy',
-        children: '0',
-        infants: '0',
-        includeCarriers: 'VH;LA',
-        excludeCarriers: 'AV;EX',
-        country: 'CO',
-        currency: 'COP',
-        locale: 'es-MX',
-        originPlace: ciudadorigen  + "-sky",
-        destinationPlace: ciudaddestino + "-sky",
-        outboundDate: fechaida,
-        adults: pasajeros
-    };
+var endPoints = {
+    'buildSearch': 'https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/pricing/v1.0/',
+    'getSearchData': 'https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/pricing/uk2/v1.0/'
+};
 
-    var promise = new Promise(function (resolve, reject) {
-        request.post({
-            url: endPoints.buildSearch,
-            headers: headers,
-            form: formData
+var headers = {
+    'X-RapidAPI-Host': "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
+    'X-RapidAPI-Key': "ccd0998f3amsh10e1fed69fc31afp14386ejsnb4fdf587810c"
+};
+
+var formData = {
+    inboundDate: fechavuelta,
+    cabinClass: 'economy',
+    children: '0',
+    infants: '0',
+    includeCarriers: 'VH;LA',
+    excludeCarriers: 'AV;EX',
+    country: 'CO',
+    currency: 'COP',
+    locale: 'es-MX',
+    originPlace: ciudadorigen  + "-sky",
+    destinationPlace: ciudaddestino + "-sky",
+    outboundDate: fechaida,
+    adults: pasajeros
+};
+
+var promise = new Promise(function (resolve, reject) {
+    request.post({
+        url: endPoints.buildSearch,
+        headers: headers,
+        form: formData
+    }, function (error, response, body) {
+        if (!error && response.statusCode === 201) {
+            //RateLimit 400 x Minuto = 429
+            var loc = response.headers.location;
+            var locSplit = loc.split('/');
+            var key = locSplit[locSplit.length - 1];
+
+            getPricingWithKey(key, 'price', 'asc', 0, 0, 5);
+        } else {
+            reject({
+                error: error,
+                response: response,
+                body: body
+        });
+    }
+})
+
+function getPricingWithKey(key, sortType, sortOrder, stops, pageIndex, pageSize) {
+    var url = endPoints.getSearchData;
+    url += key;
+    url += '?sortType=' + sortType;
+    url += '&sortOrder=' + sortOrder;
+    url += '&stops=' + stops;
+    url += '&pageIndex=' + pageIndex;
+    url += '&pageSize=' + pageSize;
+
+    request.get({
+        url: url,
+        headers: headers,
         }, function (error, response, body) {
-            if (!error && response.statusCode === 201) {
-                //RateLimit 400 x Minuto = 429
-                var loc = response.headers.location;
-                var locSplit = loc.split('/');
-                var key = locSplit[locSplit.length - 1];
-            
-                getPricingWithKey(key, 'price', 'asc', 0, 0, 5);
+            if (!error && response.statusCode === 200) {
+                //RateLimit 400 x Minuto = 410
+                var j = JSON.parse(body);
+                var dataClaro = stripData(j);
+                resolve(dataClaro);
             } else {
                 reject({
                     error: error,
                     response: response,
                     body: body
-            });
-        }
-    })
+                });
+            }
+        })
+    }
+});
 
-    function getPricingWithKey(key, sortType, sortOrder, stops, pageIndex, pageSize) {
-        var url = endPoints.getSearchData;
-        url += key;
-        url += '?sortType=' + sortType;
-        url += '&sortOrder=' + sortOrder;
-        url += '&stops=' + stops;
-        url += '&pageIndex=' + pageIndex;
-        url += '&pageSize=' + pageSize;
-        
-        request.get({
-            url: url,
-            headers: headers,
-            }, function (error, response, body) {
-                if (!error && response.statusCode === 200) {
-                    //RateLimit 400 x Minuto = 410
-                    var j = JSON.parse(body);
-                    var dataClaro = stripData(j);
-                    resolve(dataClaro);
-                } else {
-                    reject({
-                        error: error,
-                        response: response,
-                        body: body
-                    });
-                }
-            })
-        }
-    });
-
-    function stripData(dataClaro){   
-        var filteredData = [];
-        var result = dataClaro.Itineraries;
-     
-        for(var i=0; i<result.length; i++){
-            var tempObj = {
-                InboundLegId: result[i].InboundLegId,
-                OutboundLegId: result[i].OutboundLegId,
-                price: result[i].PricingOptions[0].Price
-            };
-            filteredData.push(tempObj)
-        }
-        
-        return filteredData
-     
+function stripData(dataClaro){   
+    var filteredData = [];
+    var result = dataClaro.Itineraries;
+    
+    for(var i=0; i<result.length; i++){
+        var tempObj = {
+            InboundLegId: result[i].InboundLegId,
+            OutboundLegId: result[i].OutboundLegId,
+            price: result[i].PricingOptions[0].Price
         };
-        
+        filteredData.push(tempObj)
+    }
+
+    return filteredData
+
+};
+
     return promise.then(function(filteredData){
         return filteredData
         //console.log(filteredData)
     });
+
 }
--- luca_sky_precioenvivo_get.js
+++ luca_sky_precioenvivo_get.js
@@ -1,51 +1,52 @@
-/*main() will be run when you invoke this action
- *@param Cloud Functions actions accept a single parameter, which must be a JSON object.
- *@return The output of this action, which must be a JSON object.*/
+/*
+ * main() will be run when you invoke this action
+ * @param Cloud Functions actions accept a single parameter, which must be a JSON object.
+ * @return The output of this action, which must be a JSON object.
+ */
 
 function main(params) {
 
-    var request = require('request');
-    var key_post  = '5e7ebd89-5b6a-434d-9440-62344d8fc286';
-    var sortType = 'price';
-    var sortOrder= 'asc';
-    var stops = '0';
-    var pageIndex = '0';
-    var pageSize = '5';
+var request = require('request');
+var key_post  = '5e7ebd89-5b6a-434d-9440-62344d8fc286';
+var sortType = 'price';
+var sortOrder= 'asc';
+var stops = '0';
+var pageIndex = '0';
+var pageSize = '5';
 
-    var getSearchData = "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/pricing/uk2/v1.0/";
-    
-    var headers = {
-        'X-RapidAPI-Host': "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
-        'X-RapidAPI-Key': "ccd0998f3amsh10e1fed69fc31afp14386ejsnb4fdf587810c"
-    };
-    
-    var url = getSearchData;
-    url += key_post;
-    url += '?sortType=' + sortType;
-    url += '&sortOrder=' + sortOrder;
-    url += '&stops=' + stops;
-    url += '&pageIndex=' + pageIndex;
-    url += '&pageSize=' + pageSize;
-    
-    
-    var promise = new Promise(function (resolve, reject) {
-        request.get({
-            url: url,
-            headers: headers,
-        }, function (error, response, body) {
-            if (!error && response.statusCode === 200) {
-                var j = JSON.parse(body);
-                resolve(j);
-            } else {
-                reject({
-                    error: error,
-                    response: response,
-                    body: body
-                });
-            }
-        })
-    });
-  
-    return promise
-    
+var getSearchData = "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/pricing/uk2/v1.0/";
+
+var headers = {
+    'X-RapidAPI-Host': "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
+    'X-RapidAPI-Key': "ccd0998f3amsh10e1fed69fc31afp14386ejsnb4fdf587810c"
+};
+
+var url = getSearchData;
+url += key_post;
+url += '?sortType=' + sortType;
+url += '&sortOrder=' + sortOrder;
+url += '&stops=' + stops;
+url += '&pageIndex=' + pageIndex;
+url += '&pageSize=' + pageSize;
+
+var promise = new Promise(function (resolve, reject) {
+    request.get({
+        url: url,
+        headers: headers,
+    }, function (error, response, body) {
+        if (!error && response.statusCode === 200) {
+            var j = JSON.parse(body);
+            resolve(j);
+        } else {
+            reject({
+                error: error,
+                response: response,
+                body: body
+            });
+        }
+    })
+});
+
+return promise
+
 }
